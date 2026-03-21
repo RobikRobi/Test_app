8f817f85c5c9cf6398142ff36c68eca2d8fc2db4
admin <denis.lamochenko@gmail.com>
2026-03-21 19:16:55 +0800

    feature: switch server

--- src/redis_client.py
+++ src/redis_client.py
@@ -3,8 +3,17 @@ from src.config import config
 
 
 
-redis_client = redis.Redis(host=config.env_data.REDIS_HOST,
-    # password=config.env_data.REDIS_PASSWORD,
+# redis_client = redis.Redis(host=config.env_data.REDIS_HOST,
+#     # password=config.env_data.REDIS_PASSWORD,
+#     port=config.env_data.REDIS_PORT,
+#     db=0
+# )
+
+redis_client = redis.Redis(
+    host=config.env_data.REDIS_HOST,
+    password=config.env_data.REDIS_PASSWORD,
     port=config.env_data.REDIS_PORT,
-    db=0
+    db=0,
+    ssl=True,
+    ssl_ca_certs="/.redis/root.crt",
 )
